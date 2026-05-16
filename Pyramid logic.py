f45b196486945c3d473ef0a7c39af1557f9b857f
Akshaj-Bitt <akshaj200410@gmail.com>
2026-05-16 11:44:37 +0530

    Create Pyramid logic.py

---
+++ Pyramid logic.py
@@ -0,0 +1,10 @@
+rows = 10
+for i in range(1, rows + 1):
+    spaces = ' ' * (rows - i)
+    stars = '*' * (2 * i - 1)
+    print(spaces + stars)
+   
+
+#n=5
+#for i in range (1,6):
+ #  print(" " "*"*(n-i)
